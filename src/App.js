fe9c16294d966e05d85cb3e7ddbae3f2a08f37dc
Ernest Tidalgo <etidalgo@gmail.com>
2017-09-05 23:35:46 +1200

    Copy code from react-reorderable demo

--- src/App.js
+++ src/App.js
@@ -1,19 +1,42 @@
 import React, { Component } from 'react';
+import ReactReorderable from 'react-reorderable';
 import logo from './logo.svg';
 import './App.css';
+
+function addRow(data) {
+  var el = document.getElementById('events');
+  el.innerHTML += '<br>' + data;
+}
+
+function getLabel(node) {
+  return node.props.children[1].props.children
+}
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <ReactReorderable 
+        handle='.draggable-handle'
+        mode='grid'
+        onDragStart= { function (data) {
+          addRow('Drag start: ' + getLabel(data.props.children));
+        }}
+        onDrop={function (data) {
+          addRow('Drop: ' + data.map(getLabel).join(', '));
+        }}
+        onChange={function (data) {
+          addRow('Change: ' + data.map(getLabel).join(', '));
+        }}
+      >
+      <div
+        className='draggable-element'>
+        <div className='draggable-handle'>1</div>
       </div>
+      <div
+        className='draggable-element'>
+        <div className='draggable-handle'>2</div>
+      </div>
+    </ReactReorderable>
     );
   }
 }
